83412c408d45b5f53754e8000683928d62e98c4e
crojo19 <techcronkite@gmail.com>
2020-10-26 17:17:21 -0500

    add /status to get full URL site map

--- main/application.py
+++ main/application.py
@@ -26,4 +26,35 @@ def index(req, resp):
                 yield from resp.awrite(str(mount.url) + str(item[0]))
                 yield from resp.awrite("</p>")
 
+@site.route("/config")
+def config(req, resp):
+    # Create based on config file
+    data = configure.read_config_file()
+    # add Mac address
+    data.update({'mac_address': ubinascii.hexlify(network.WLAN().config('mac'), ':').decode()})
+    # add ip address
+    (ip, other, other1, other2) = network.WLAN().ifconfig()
+    data.update({'ip_address': ip})
+    # get current installed app version
+    o = OTAUpdater(configure.read_config_file("update_repo"))
+    current_version = o.get_current_version()
+    data.update({'installed_version': current_version})
+
+    wifi = wifimgr.read_profiles()
+    data.update({'wifi': wifi})
+
+    items = {}
+    i = 0
+    for item in site.get_url_map():
+        if str(item[0]).startswith('/'):
+            items.update({i: str(item[0])})
+            i = i + 1
+    for mount in site.get_mounts():
+        for item in mount.get_url_map():
+            if not str(item[0]).endswith("/"):
+                items.update({i: str(item[0])})
+                i = i + 1
+    data.update({'admin_routes': items})
+    return data
+
 site.run(host='0.0.0.0',debug=True, port=80)
