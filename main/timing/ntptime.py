1a074fced2408950ac21d0c9720f2a515badc3be
crojo19 <techcronkite@gmail.com>
2020-11-26 10:12:56 -0600

    alternate back to normal

--- main/timing/ntptime.py
+++ main/timing/ntptime.py
@@ -34,6 +34,7 @@ def settime():
     t, micro_sec = time()
     import machine
     import utime
-    print(t)
+    print("server: " + str(host) + " Time: " + str(t))
+
     tm = utime.gmtime(t)
     machine.RTC().datetime((tm[0], tm[1], tm[2], tm[6] + 1, tm[3], tm[4], tm[5], micro_sec))
