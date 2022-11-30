69040b790407432ff208304bb6c04cc0a511272a
crojo19 <techcronkite@gmail.com>
2022-11-29 21:23:58 -0600

    updating error module to provide more consise information

--- main/error.py
+++ main/error.py
@@ -2,11 +2,11 @@ import machine
 import urequests
 import ujson
 import time
-
+import network
 
 def write_error(error):
     with open("error.log", "a") as logf:
-        logf.write(str(time.gmtime()) + " - " + str(error))
+        logf.write(str(time.gmtime()) + "-" + str(error))
 
 
 def send_error(server_ip=None, server_port=80):
@@ -30,11 +30,13 @@ def send_error(server_ip=None, server_port=80):
     except OSError as e:
         write_error(e)
         pass
+
+    (ip, other, other1, other2) = network.WLAN().ifconfig()
     try:
         if len(errors) > 0:
-            data = ujson.dumps({'reset_code': reset, 'error_count': len(errors), 'error': errors})
+            data = ujson.dumps({'reset_code': reset, 'error_count': len(errors), 'error': errors, 'ip': ip})
         if len(errors) == 0:
-            data = ujson.dumps({'reset_code': reset, 'error_count': 0})
+            data = ujson.dumps({'reset_code': reset, 'error_count': 0, 'ip': ip})
         urequests.post(url, headers=pb_headers, json=data)
     except Exception as e:
         write_error(e)
